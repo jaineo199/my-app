fa2bf230c407232ac7ef33e4c7d950936bb80463
Jai Sarki <jai.sarki@fospha.com>
2023-11-07 17:14:42 +0530

    added

--- src/components/BubbleChartTwo.tsx
+++ src/components/BubbleChartTwo.tsx
@@ -14,14 +14,16 @@ const BubbleChart = () => {
       {
         name: "Sample Data",
         data: [
-          { x: 1, y: 20, z: 10, fillColor: "#C9232380" },
-          { x: 2, y: 15, z: 30, fillColor: "#FFE60080" },
+          { x: "", y: 0, z: 0, fillColor: "#C9232380" },
+          { x: 1, y: 20, z: 40, fillColor: "#C9232380" },
+          { x: 2, y: 15, z: 50, fillColor: "#FFE60080" },
           { x: 3, y: 25, z: 35, fillColor: "#45D13680" },
           { x: 4, y: 30, z: 25, fillColor: "#45D13680" },
           { x: 5, y: 10, z: 45, fillColor: "#C9232380" },
           { x: 6, y: 35, z: 20, fillColor: "#FFE60080" },
           { x: 7, y: 45, z: 10, fillColor: "#45D13680" },
           { x: 8, y: 37, z: 10, fillColor: "#45D13680" },
+          { x: 9, y: 0, z: 0, fillColor: "#45D13680" },
         ],
       },
     ],
@@ -47,7 +49,8 @@ const BubbleChart = () => {
     grid: {
       show: false,
       padding: {
-        left: 30,
+        left: 10,
+        right: 10,
       },
     },
     tooltip: {
@@ -105,7 +108,7 @@ const BubbleChart = () => {
         series={options.series}
         type="bubble"
         height={600}
-        width={500}
+        width={1000}
       />
     </div>
   );
