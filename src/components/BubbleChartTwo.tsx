b21e9932cb225f3f3cd63dc0fd6cf3ea8af1ccf3
Jai Sarki <jai.sarki@fospha.com>
2023-11-07 12:45:04 +0530

    fixed color

--- src/components/BubbleChartTwo.tsx
+++ src/components/BubbleChartTwo.tsx
@@ -14,21 +14,21 @@ const BubbleChart = () => {
       {
         name: "Sample Data",
         data: [
-          [1, 20, 40],
-          [2, 15, 30],
-          [3, 25, 35],
-          [4, 30, 25],
-          [5, 10, 45],
-          [6, 35, 20],
-          [7, 45, 10],
-          [8, 37, 10],
+          { x: 1, y: 20, z: 40, fillColor: "#C9232380" },
+          { x: 2, y: 15, z: 30, fillColor: "#FFE60080" },
+          { x: 3, y: 25, z: 35, fillColor: "#45D13680" },
+          { x: 4, y: 30, z: 25, fillColor: "#45D13680" },
+          { x: 5, y: 10, z: 45, fillColor: "#C9232380" },
+          { x: 6, y: 35, z: 20, fillColor: "#FFE60080" },
+          { x: 7, y: 45, z: 10, fillColor: "#45D13680" },
+          { x: 8, y: 37, z: 10, fillColor: "#45D13680" },
         ],
       },
     ],
     xaxis: {
       tickAmount: 8,
       title: {
-        text: "X-Axis",
+        text: "Revenue",
       },
       axisBorder: {
         show: false,
@@ -40,7 +40,7 @@ const BubbleChart = () => {
     yaxis: {
       tickAmount: 5,
       title: {
-        text: "Y-Axis",
+        text: "CPP",
       },
     },
     grid: {
