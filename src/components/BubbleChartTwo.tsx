3fbe3f88525fa7268cec43830eb242e6290796dc
Jai Sarki <jai.sarki@fospha.com>
2023-11-09 11:40:51 +0530

    done

--- src/components/BubbleChartTwo.tsx
+++ src/components/BubbleChartTwo.tsx
@@ -14,16 +14,20 @@ const BubbleChart = () => {
       {
         name: "Sample Data",
         data: [
-          { x: "", y: 0, z: 0, fillColor: "#C9232380" },
-          { x: 1, y: 20, z: 40, fillColor: "#C9232380" },
-          { x: 2, y: 15, z: 50, fillColor: "#FFE60080" },
-          { x: 3, y: 25, z: 35, fillColor: "#45D13680" },
-          { x: 4, y: 30, z: 25, fillColor: "#45D13680" },
-          { x: 5, y: 10, z: 45, fillColor: "#C9232380" },
-          { x: 6, y: 35, z: 20, fillColor: "#FFE60080" },
-          { x: 7, y: 45, z: 10, fillColor: "#45D13680" },
-          { x: 8, y: 37, z: 10, fillColor: "#45D13680" },
-          { x: 9, y: 0, z: 0, fillColor: "#45D13680" },
+          // { x: 1, y: 20, z: 40, fillColor: "#C9232380" },
+          // { x: 2, y: 15, z: 50, fillColor: "#FFE60080" },
+          // { x: 3, y: 25, z: 35, fillColor: "#45D13680" },
+          // { x: 4, y: 30, z: 25, fillColor: "#45D13680" },
+          // { x: 5, y: 10, z: 45, fillColor: "#C9232380" },
+          // { x: 6, y: 35, z: 20, fillColor: "#FFE60080" },
+          // { x: 7, y: 45, z: 10, fillColor: "#45D13680" },
+          // { x: 8, y: 37, z: 10, fillColor: "#45D13680" },
+
+          { x: 1842733.3, y: 0.2, z: 4582.8 },
+
+          { x: 1342733.3, y: 3.2, z: 382.8 },
+          { x: 142733.3, y: 7.2, z: 82.8 },
+          { x: 242733.3, y: 5.2, z: 322.8 },
         ],
       },
     ],
@@ -48,10 +52,10 @@ const BubbleChart = () => {
     },
     grid: {
       show: false,
-      padding: {
-        left: 10,
-        right: 10,
-      },
+      // padding: {
+      //   left: 10,
+      //   right: 10,
+      // },
     },
     tooltip: {
       enabled: true,
@@ -108,7 +112,7 @@ const BubbleChart = () => {
         series={options.series}
         type="bubble"
         height={600}
-        width={1000}
+        width={"100%"}
       />
     </div>
   );
